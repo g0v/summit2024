ac26a65d227010c3dee5f8a46a14edf637274752
柴犬 <shiba@shibasalmondeMacBook-Pro.local>
2024-01-06 13:26:04 +0800

    [New] Move assets to root and change build path.

--- gulpfile.js
+++ gulpfile.js
@@ -4,41 +4,50 @@ const sass = require('gulp-sass')(require('sass'));
 const data = require('gulp-data');
 const connect = require('gulp-connect');
 const sitemap = require('gulp-sitemap');
+const path = require('path')
 
-function buildSass() {
+function buildSass(baseurl="/") {
+  let dest_path = '.' + path.join('/static/', baseurl, '/assets/css')
   gulp.src('src/sass/**/*.sass')
     .pipe(data((file) => {
       console.log("[build] " + file['history']);
     }))
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./static/assets/css'))
+    .pipe(gulp.dest(dest_path))
+    .pipe(connect.reload());
+
+  gulp.src('./assets/**')
+    .pipe(gulp.dest('.' + path.join('/static/', baseurl, '/assets/')))
     .pipe(connect.reload());
 }
 
 function buildPug(baseurl="/") {
   let build_time = new Date().getTime();
+  let dest_path = '.' + path.join('/static/', baseurl)
+  console.log(dest_path)
   gulp.src('src/pug/**/index.pug')
     .pipe(data((file) => {
       console.log("[build] " + file['history']);
       const result = {
         index: require('./src/data/index.json'),
-        timestamp: build_time
+        timestamp: build_time,
+        base: baseurl
       };
       return result;
     }))
     .pipe(pug())
-    .pipe(gulp.dest('./static/'))
+    .pipe(gulp.dest(dest_path))
     .pipe(sitemap({
       siteUrl: 'https://summit.g0v.tw',
       images: true
     }))
-    .pipe(gulp.dest('./static/'))
+    .pipe(gulp.dest(dest_path))
     .pipe(connect.reload());
 }
 
 gulp.task('build', async() => {
-  await buildSass();
-  await buildPug();
+  await buildSass('/2024/');
+  await buildPug('/2024/');
 });
 
 gulp.task('server', function () {
